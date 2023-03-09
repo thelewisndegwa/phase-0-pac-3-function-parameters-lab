498a5efac772113bf3d7d0a4a31e069e41236437
thelewisndegwa <lewiskanyi77@gmail.com>
2023-03-09 08:32:42 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+// function introduction(name){
+//     return "Hi my name is" +name
+// }
+
+// introduction("Aki")
+
+
+// function introductionWithLanguage(name, language){
+// return("Hi, my name is .${name} and I am learning to program in ${language}")
+// }
+// introductionWithLanguage("Aki","Ember.js")
+
+
+// function introductionWithLanguageOptional(name, language){
+//  return("Hi, my name is ${name} and I am learning to program in ${language}")
+// }
+
+// introductionWithLanguageOptional("Gracie","Javascript")
+// introductionWithLanguageOptional("Gracie","Python")
+
+
+function introduction (name){
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage (name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional (name, language='JavaScript'){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
